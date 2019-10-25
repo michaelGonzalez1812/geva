7663aaebfea08965c535382d2357495e0821f926
Jorge Aguero Zamora <jorge.aguerozamora@cheetahdigital.com>
2019-10-25 02:31:50 -0600

    deleting rogue source file

--- rightRotate_n.sv
+++
@@ -1,9 +0,0 @@
-module rightRotate_n #(parameter N=8)
-				(input  logic [N-1:0] B, 
-             input  logic [$clog2(N)-1:0]  A, 
-             output logic [N-1:0] Y);
-                 
-            
-			assign Y = {B,B} >> A;	 
-				 
-endmodule
