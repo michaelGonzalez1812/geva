242cf846df34a081c840e0952b756145731a41a9
Erick Cordero <eguicoro2@gmail.com>
2019-10-26 19:14:00 -0600

    hex image  generator

---
+++ Imagenes/init_mem.py
@@ -0,0 +1,23 @@
+def remake(num):
+    while(len(num) < 4):
+        num = '0'+num
+    return num
+
+def init_mem():
+    #lectura del archivo
+    open('init.hex', 'w').close()
+    direccion = 0
+    f1 = open("init.hex", "a+")
+    cont = 0
+    while(cont <= 2047):
+        data = "0000000000000000000000000000000000000000000000000000000000000000000"
+        direccionHex = remake(hex(direccion)[2:])
+        f1.write(":20"+direccionHex+"00"+data+"\n")
+        #print(":20"+str(direccionHex)+"00"+data)
+        direccion += 32
+        cont += 1
+    f1.write(":0A0080000000000000000000000080")
+    f1.close()
+
+
+init_mem()
